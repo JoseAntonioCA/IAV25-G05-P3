5b4a52aa9166740d657436ae6ff32f499a2156ba
Javalonso1 <123580168+Javalonso1@users.noreply.github.com>
2025-04-26 20:43:23 +0200

    Scirpts acabados en teoria

--- Assets/NarratechLaboratories/GhostBase/AI/MainMovement/MoveToExit.cs
+++ Assets/NarratechLaboratories/GhostBase/AI/MainMovement/MoveToExit.cs
@@ -3,8 +3,29 @@ using System.Collections.Generic;
 using UnityEngine;
 using BehaviorDesigner.Runtime;
 using BehaviorDesigner.Runtime.Tasks;
+using UnityEngine.AI;
 
 public class MoveToExit : Action
 {
-    
+    public SharedVector3 exit;
+    private NavMeshAgent navMeshAgent;
+
+    public override void OnAwake()
+    {
+        navMeshAgent = transform.gameObject.GetComponent<NavMeshAgent>();
+    }
+
+    public override void OnStart()
+    {
+        navMeshAgent.SetDestination(exit.Value);
+    }
+
+    public override TaskStatus OnUpdate()
+    {
+        if (Vector3.SqrMagnitude(transform.position - exit.Value) < 0.01f)
+        {
+            return TaskStatus.Success;
+        }
+        return TaskStatus.Running;
+    }
 }
